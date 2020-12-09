f49682662814efac097dd4e4ced1fd54ebfd0444
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2020-12-09 17:15:37 +0000

    added log in fragment, need to link log in to firebase

--- app/src/main/java/com/example/cs4125_project/MainActivity.java
+++ app/src/main/java/com/example/cs4125_project/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.cs4125_project;
 
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,14 +25,16 @@ import com.google.firebase.firestore.Query;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ProductInterfaceAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpRecyclerView();
+        findViewById(R.id.logInBtn).setOnClickListener(this);
     }
 
     private void setUpRecyclerView() {
@@ -34,5 +43,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+    }
+
+    public void goToLogIn(View v)
+    {
+        Fragment fr = new LogInFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.content, fr);
+        fragmentTransaction.commit();
+    }
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.logInBtn) {
+            //Log in method logIn();
+            goToLogIn(v);
+        }
     }
 }
